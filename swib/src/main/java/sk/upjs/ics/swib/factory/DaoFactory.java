6b8478af429887d9f592f74513351e2a4364fc6e
kulexpipiens <kulexpipiens@centrum.sk>
2015-10-19 23:18:37 +0200

    upravena factory

--- swib/src/main/java/sk/upjs/ics/swib/factory/DaoFactory.java
+++ swib/src/main/java/sk/upjs/ics/swib/factory/DaoFactory.java
@@ -20,12 +20,12 @@ public enum DaoFactory {
     public DataSource dataSource() {
         MysqlDataSource dataSource = new MysqlDataSource();
         if ("true".equals(System.getProperty("testovaciRezim"))) {
-            dataSource.setURL("jdbc:mysql://www.db4free.net:3306/swib_test");
-            dataSource.setUser("swib_test");
+            dataSource.setURL("jdbc:mysql://46.229.230.241:3306/hq007701db");
+            dataSource.setUser("hq007700");
             dataSource.setPassword("NaseTimoveHeslo2015");
         } else {
-            dataSource.setURL("jdbc:mysql://www.db4free.net:3306/swib");
-            dataSource.setUser("swib");
+            dataSource.setURL("jdbc:mysql://46.229.230.241:3306/hq007700db");
+            dataSource.setUser("hq007700");
             dataSource.setPassword("NaseTimoveHeslo2015");
         }
 
