faeba0406f02727c7923accf188845a1bf4099a2
dpalisinova <chansy41@gmail.com>
2015-11-05 11:08:49 +0100

    Uprava UverDao.java
    este import som opravila

--- swib/src/main/java/sk/upjs/ics/swib/dao/UverDao.java
+++ swib/src/main/java/sk/upjs/ics/swib/dao/UverDao.java
@@ -1,7 +1,7 @@
 package sk.upjs.ics.swib.dao;
 
 import java.util.List;
-import sk.upjs.ics.swib.entity.Uvet;
+import sk.upjs.ics.swib.entity.Uver;
 
 public interface UverDao {
     List<Uver> dajVsetky();
