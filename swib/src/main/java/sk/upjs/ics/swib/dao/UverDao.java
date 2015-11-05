4bffbd696944d72b0bec1a8535f898ddc2b53222
dpalisinova <chansy41@gmail.com>
2015-11-05 11:07:22 +0100

    Uprava UverDao.java
    Upravila som hlavicky metod a importy, lebo boli napisane pre Ucet.java nie pre Uver.java

--- swib/src/main/java/sk/upjs/ics/swib/dao/UverDao.java
+++ swib/src/main/java/sk/upjs/ics/swib/dao/UverDao.java
@@ -1,11 +1,11 @@
 package sk.upjs.ics.swib.dao;
 
 import java.util.List;
-import sk.upjs.ics.swib.entity.Ucet;
+import sk.upjs.ics.swib.entity.Uvet;
 
 public interface UverDao {
-    List<Ucet> dajVsetky();
-    void pridaj(Ucet ucet);
-    void odstran(Ucet ucet);
-    void uprav(Ucet ucet);
+    List<Uver> dajVsetky();
+    void pridaj(Uver uver);
+    void odstran(Uver uver);
+    void uprav(Uver uver);
 }
