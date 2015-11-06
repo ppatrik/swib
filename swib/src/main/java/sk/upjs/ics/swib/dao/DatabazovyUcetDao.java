81ba52d8e7803cbf43763e9dcad8bef234bfd4e6
Patrik Pekarčík <patrik@recepcia-online.sk>
2015-11-06 01:25:14 +0100

    oprava chybneho merge

--- swib/src/main/java/sk/upjs/ics/swib/dao/DatabazovyUcetDao.java
+++ swib/src/main/java/sk/upjs/ics/swib/dao/DatabazovyUcetDao.java
@@ -1,37 +1,7 @@
-<<<<<<< HEAD
-
 package sk.upjs.ics.swib.dao;
 
 import java.util.List;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 
-import sk.upjs.ics.swib.entity.Ucet;
-
-
-public class DatabazovyUcetDao implements UcetDao{
-private JdbcTemplate jdbcTemplate;
-    private BeanPropertyRowMapper<Ucet> mapovac
-            = new BeanPropertyRowMapper<>(Ucet.class);
-    
-     public DatabazovyUcetDao(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-     }
-    @Override
-    public List<Ucet> dajVsetky() {
-        String sql = "SELECT *FROM Ucet";
-        return jdbcTemplate.query(sql, mapovac);
-         }
-
-    @Override
-    public void pridaj(Ucet ucet) {
-        String sql = "INSERT INTO Ucet VALUES (?, ?, ?, ?, ?, ?)";
-        jdbcTemplate.update(sql, null, ucet.getKlientId(), ucet.getName(), ucet.getZostatok(), ucet.getCisloUctu(), ucet.isSpor());
-          }
-=======
-package sk.upjs.ics.swib.dao;
-
-import java.util.List;
 import org.springframework.jdbc.core.JdbcTemplate;
 import sk.upjs.ics.swib.entity.Ucet;
 import sk.upjs.ics.swib.mappers.UcetRowMapper;
@@ -56,24 +26,13 @@ public class DatabazovyUcetDao implements UcetDao {
     @Override
     public void pridaj(Ucet ucet) {
         String sql = "INSERT INTO Ucet VALUES (? ,?, ?, ?, ?, ?)";
-        jdbcTemplate.update(sql, null , ucet.getKlientId(), ucet.getName(), ucet.getZostatok(), ucet.getCisloUctu(), ucet.isSpor() ? 1 : 0);
+        jdbcTemplate.update(sql, null, ucet.getKlientId(), ucet.getName(), ucet.getZostatok(), ucet.getCisloUctu(), ucet.isSpor() ? 1 : 0);
     }
->>>>>>> origin/master
 
     @Override
     public void odstran(Ucet ucet) {
         String sql = "DELETE FROM Ucet WHERE ID = ?";
         jdbcTemplate.update(sql, ucet.getId());
-<<<<<<< HEAD
-          }
-
-    @Override
-    public void uprav(Ucet ucet) {
-         String sql = "UPDATE Ucet SET KlientID = ?, Nazov = ?, Zostatok = ?, C_uctu = ?, Spor = ? WHERE ID = ?";
-         jdbcTemplate.update(sql,  ucet.getKlientId(),ucet.getName(), ucet.getZostatok(), ucet.getCisloUctu(), ucet.isSpor());
-       }
-    
-=======
     }
 
     @Override
@@ -82,5 +41,4 @@ public class DatabazovyUcetDao implements UcetDao {
         jdbcTemplate.update(sql, ucet.getKlientId(), ucet.getName(), ucet.getZostatok(), ucet.getCisloUctu(), ucet.isSpor() ? 1 : 0, ucet.getId());
     }
 
->>>>>>> origin/master
 }
