fb34a20451530e4dd239d5a5bf2ff04322d46a53
JParaska <janyyyy@centrum.sk>
2015-11-12 15:46:53 +0100

    JDKlientInfo JFZoznamKlientov KlientTablemodel UctyTableModel

--- swib/src/main/java/sk/upjs/ics/swib/gui/KlientTableModel.java
+++ swib/src/main/java/sk/upjs/ics/swib/gui/KlientTableModel.java
@@ -7,16 +7,25 @@ package sk.upjs.ics.swib.gui;
 
 import java.util.*;
 import javax.swing.table.AbstractTableModel;
+import org.springframework.jdbc.core.JdbcTemplate;
 import sk.upjs.ics.swib.entity.Klient;
 import sk.upjs.ics.swib.guiDAOTest.zoznamKlientov;
+import sk.upjs.ics.swib.dao.DatabazovyKlientDao;
+import sk.upjs.ics.swib.dao.DatabazovyUcetDao;
+import sk.upjs.ics.swib.entity.Ucet;
+import sk.upjs.ics.swib.factory.DaoFactory;
 
 /**
  *
  * @author Uživateľ
  */
-public class KlientTableModel extends AbstractTableModel {   
+public class KlientTableModel extends AbstractTableModel {  
     
-    private List<Klient> zoznamK = zoznamKlientov.getList();
+    private final JdbcTemplate jdbcTemplate = DaoFactory.INSTANCE.jdbcTemplate();
+    private final DatabazovyKlientDao databazovyKlientDao = new DatabazovyKlientDao(jdbcTemplate);
+    private final DatabazovyUcetDao databazovyUcetDao = new DatabazovyUcetDao(jdbcTemplate);
+    
+    private List<Klient> zoznamK = databazovyKlientDao.dajVsetkych();
     
     private static final int COLUMN_NUMBER = 2;
     private static final String[] COLUMN_TITLE = {"Celé meno", "ID"};
@@ -25,14 +34,17 @@ public class KlientTableModel extends AbstractTableModel {
         Integer.class
     };
 
+    @Override
     public int getRowCount() {
         return zoznamK.size();
     }
 
+    @Override
     public int getColumnCount() {
         return COLUMN_NUMBER;
     }
 
+    @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
         Klient vybranyKlient = zoznamK.get(rowIndex);
         if (columnIndex == 0) {
@@ -45,7 +57,7 @@ public class KlientTableModel extends AbstractTableModel {
     }            
     
     public void refresh(){
-        zoznamK = zoznamKlientov.getList();
+        zoznamK = databazovyKlientDao.dajVsetkych();
         fireTableDataChanged();
     }
 
@@ -56,5 +68,13 @@ public class KlientTableModel extends AbstractTableModel {
     
     public Klient getKlient(int index) {
         return zoznamK.get(index);
+    }
+
+    void zmaz(Klient vybranyKlient) {       
+        for (Ucet ucet : databazovyUcetDao.dajVsetky(vybranyKlient)){
+            databazovyUcetDao.odstran(ucet);
+        }
+        databazovyKlientDao.odstran(vybranyKlient);
+        refresh();
     }
 }
